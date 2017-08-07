eb9e7c67b281c3df00847c55227dccf81304d8d5
karthik530 <bkarthik530@gmail.com>
2017-08-07 21:07:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one/',function(req,res){
-res.send("Arthicle requested will be server")
+res.send("Article requested will be served")
 });
 app.get('/article-two/',function(req,res){
 res.send("Arthicle requested will be server")
